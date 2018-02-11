e7101b3fa522460337d686a4c1a7e4b0a99ccde3
alphagocc <alphagocc@163.com>
2018-02-11 16:06:41 +0800

    Create .ts file

---
+++ en_US.ts
@@ -0,0 +1,88 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>FiveChess</name>
+    <message>
+        <location filename="FiveChess.ui" line="38"/>
+        <source>New Game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="FiveChess.ui" line="51"/>
+        <source>Load Game</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="FiveChess.ui" line="14"/>
+        <location filename="FiveChess.ui" line="63"/>
+        <source>FiveChess</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>battleGui</name>
+    <message>
+        <location filename="battlegui.ui" line="20"/>
+        <source>Battle</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="battlegui.ui" line="65"/>
+        <source>Exit</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="battlegui.ui" line="72"/>
+        <source>Save</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="battlegui.ui" line="79"/>
+        <source>Time：</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="battlegui.cpp" line="20"/>
+        <source>Time：%1 Second</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>chessBoardData_Process</name>
+    <message>
+        <location filename="chessboarddata_process.cpp" line="61"/>
+        <source>Save Chess Board File</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="chessboarddata_process.cpp" line="63"/>
+        <location filename="chessboarddata_process.cpp" line="96"/>
+        <source>ChessBoardFile(*.chessbrd)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="chessboarddata_process.cpp" line="94"/>
+        <source>Open Chess Board File</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>waitDialog</name>
+    <message>
+        <location filename="waitdialog.ui" line="14"/>
+        <source>Dialog</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="waitdialog.ui" line="20"/>
+        <source>组件开发中，敬请期待。</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="waitdialog.ui" line="27"/>
+        <source>Github：alphagocc</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
